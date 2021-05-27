7193a0d92acfe7f2b2ea1209380a88951282652e
Gangtai <gangtai.goh@gmail.com>
2021-05-27 15:14:58 +0900

    5/27 car 상속

--- eclipse-workspace/Java_Basic/src/kr/java/chapter8/inherit/CarMain.java
+++ eclipse-workspace/Java_Basic/src/kr/java/chapter8/inherit/CarMain.java
@@ -5,11 +5,14 @@ public class CarMain {
 	public static void main(String[] args) {
 		Car car = new Car("자동차", 1000);
 		car.run();
-		// car.siren();
 		
 		CarAmbulance am = new CarAmbulance();
 		am.run();
 		am.siren();
+		
+		Car car2 = new CarAmbulance();
+		car2.siren();
+
 	}
 
 }
