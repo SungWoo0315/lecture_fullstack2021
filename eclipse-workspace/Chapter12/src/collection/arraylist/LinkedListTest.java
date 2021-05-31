6b92f4005842654db0d14eb19071a3bf372588c1
Gangtai <gangtai.goh@gmail.com>
2021-05-31 17:48:53 +0900

    5/31 컬렉션프레임워크 ArrayList

---
+++ eclipse-workspace/Chapter12/src/collection/arraylist/LinkedListTest.java
@@ -0,0 +1,13 @@
+package collection.arraylist;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<String>();
+		list.add("홍길동");
+		
+	}
+
+}
